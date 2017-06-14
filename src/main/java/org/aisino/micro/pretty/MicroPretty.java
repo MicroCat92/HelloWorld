f2d2304b2bbc3a228b9f05058571927b77ee483d
micropretty <11086654@qq.com>
2017-06-14 23:22:08 +0800

    pretty update

---
+++ src/main/java/org/aisino/micro/pretty/MicroPretty.java
@@ -0,0 +1,21 @@
+package org.aisino.micro.pretty;
+
+public class MicroPretty {
+	
+	private String description;
+	
+	public void sayHi() {
+		System.out.println("Hello MicroPretty !!");
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+
+}
